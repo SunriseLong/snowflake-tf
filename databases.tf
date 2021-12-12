5e2b39266b26b6294ccbcae90c98240ebdb5ca0c
sunrise <sunrise@Sunrises-MacBook-Pro.local>
2021-12-11 16:19:41 -0800

    add db and schemas

---
+++ databases.tf
@@ -0,0 +1,6 @@
+resource "snowflake_database" "database" {
+  provider = snowflake.sys_admin
+  name                        = "WAREHOUSE_DB"
+  comment                     = "data and analytics database"
+  data_retention_time_in_days = 1
+}
